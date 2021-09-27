a5c648e941cf7e69953e05d479435c555700e13d
yyeon0802 <yyeon0802@gmail.com>
2021-09-27 15:29:34 +0900

    summary & practice - String

---
+++ 04_java_string/src/step4_01/string/StringEx08.java
@@ -0,0 +1,29 @@
+package step4_01.string;
+
+public class StringEx08 {
+
+	public static void main(String[] args) {
+		
+		// 문제) 이름과 성적을 하나의 문자열로 연결
+		// 정답) "김철수/87,이만수/42,이영희/95"
+				
+		String[] name = {"김철수", "이만수", "이영희"};
+		int[] score = {87, 42, 95};
+		
+		String str = "";
+		
+		for (int i=0; i<name.length; i++) {
+			str += name[i];
+			str += "/";
+			str += score[i] + "";
+			if (i != name.length-1) {
+				str += ",";
+			}
+		}
+
+		System.out.println(str);
+
+
+	}
+
+}
