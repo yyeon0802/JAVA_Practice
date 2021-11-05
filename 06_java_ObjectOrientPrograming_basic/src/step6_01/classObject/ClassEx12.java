5737814d288532a2082b9e51a36558f12bffae3f
yyeon0802 <yyeon0802@gmail.com>
2021-11-05 11:42:05 +0900

    summary & practice - ObjectOrientPrograming_basic

---
+++ 06_java_ObjectOrientPrograming_basic/src/step6_01/classObject/ClassEx12.java
@@ -0,0 +1,33 @@
+package step6_01.classObject;
+/* 
+ * # 틱택토
+ * === 틱택토 ===
+ * [X][X][O]
+ * [ ][O][ ]
+ * [ ][ ][ ]
+ * [p1]인덱스 입력 : 6
+ * === 틱택토 ===
+ * [X][X][O]
+ * [ ][O][ ]
+ * [O][ ][ ]
+ * [p1]승리
+ * 
+ */
+
+/*
+class Ex12{
+	String[][] game = new String[3][3];
+	
+	int turn = 0;
+	int win = 0;		
+}
+*/
+
+
+public class ClassEx12 {
+	
+	public static void main(String[] args) {
+
+	}
+
+}
